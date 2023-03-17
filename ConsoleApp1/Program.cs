4dcf4d87d9f86b07394a1e7c9923a691ac755bc6
kirill <Kirill72V@gmail.com>
2023-03-17 23:53:38 +0700

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите имя:");
+            string? name = Console.ReadLine();
+            Console.WriteLine($"{name}, Hello!");
+           
+        }
+        
+    }
+}
